67a3697471f0306d5b80be7c4c7cbb414ed3a832
Jackson Weber <jacksonweber@microsoft.com>
2024-03-27 12:03:24 -0700

    Update instrumentation.node.ts

--- my-app/instrumentation.node.ts
+++ my-app/instrumentation.node.ts
@@ -2,7 +2,7 @@ import { useAzureMonitor, AzureMonitorOpenTelemetryOptions } from "@azure/monito
 
 const options: AzureMonitorOpenTelemetryOptions = {
     azureMonitorExporterOptions: {
-      connectionString: "InstrumentationKey=b59d565e-da91-4140-8671-6c79b6938b4d;IngestionEndpoint=https://westus2-2.in.applicationinsights.azure.com/;LiveEndpoint=https://westus2.livediagnostics.monitor.azure.com/",
+      connectionString: "YOUR_INSTRUMENTATION_KEY",
     },
 }
 useAzureMonitor(options);
